41627d56fb7301635b251b5e79d7c2099036ba11
BetoAnh <vtahvp@gmail.com>
2024-10-01 20:20:45 +0700

    update home

--- fe/src/Pages/Home/Home.jsx
+++ fe/src/Pages/Home/Home.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import Header from "../../Components/header/Header";
 const Home = () => {
   return (
-    <div className="w-full bg-blue-300 h-[1000px]">
+    <div className="w-full bg-blue-300 flex justify-center">
       <Header />
-      <div
-        className="h-screen"
-        style={{
-          backgroundImage: `url('./img/testbackground.jpg')`,
-          backgroundSize: "cover",
-        }}
-      ></div>
+      <div className="max-w-[1140px] w-full">
+        <div
+          className="w-full h-[700px]"
+          style={{
+            backgroundImage: `url('./img/testbackground.jpg')`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        />
+        <div className="bg-blue-300 w-full h-[500px]" />
+      </div>
     </div>
   );
 };
